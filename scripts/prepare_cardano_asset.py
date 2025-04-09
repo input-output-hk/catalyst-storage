8fc4d13763104c450c41f4e05783ccf44393373b
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-04-10 01:23:29 +0700

    feat: prepared data

---
+++ scripts/prepare_cardano_asset.py
@@ -0,0 +1,155 @@
+"""
+This script is a simple web scraper tool to prepare testing data for the `cardano/asset` endpoint.
+"""
+
+import json
+import os
+import shlex
+import os
+import subprocess
+import time
+
+from bs4 import BeautifulSoup
+
+import address
+
+# variables
+MAX_ATTEMPT = 3
+
+# provide yours here, can acquire `CF_CLEARANCE` by going to `https://preprod.cexplorer.io`,
+# and extract this field from the cookies header using the `network` tab
+CF_CLEARANCE = ""
+USER_AGENT = ""
+
+# functions
+def request(url) -> str:
+    curl_command = f"curl '{url}' \
+    -b 'cf_clearance={CF_CLEARANCE}' \
+    -H 'user-agent: {USER_AGENT}"
+
+    args = shlex.split(curl_command)
+    result = subprocess.run(args, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        print("Error:")
+        print(result.stderr)
+        raise Exception(result.stderr)
+
+    return result.stdout
+
+def get_stake_asset_page(stake_addr: str) -> str:
+    return request(f"https://preprod.cexplorer.io/stake/{stake_addr}/asset")
+
+def get_stake_data_page(stake_addr: str) -> str:
+    return request(f"https://preprod.cexplorer.io/stake/{stake_addr}")
+
+def get_tx_page(tx) -> str:
+    return request(f"https://preprod.cexplorer.io/tx/{tx}")
+
+def get_asset_page(asset) -> str:
+    return request(f"https://preprod.cexplorer.io/asset/{asset}")
+
+# ------ INIT PROCESS ------
+
+# Read the snapshot file
+snapshot_path = os.path.join(os.path.dirname(__file__), "../snapshot-80000000-preprod.json")
+with open(snapshot_path, "r", encoding="utf-8") as f:
+    snapshot_data = json.load(f)
+
+# Process each record
+formatted_records = {}
+processing_records = snapshot_data[:]
+for (i, record) in enumerate(processing_records):
+    stake_addr = address.stake_public_key_to_address(
+        key=record["stake_public_key"][2:],
+        is_stake=True,
+        network_type="preprod"
+    )
+
+    attempt_count = 0
+
+    while attempt_count < MAX_ATTEMPT:
+        try:
+            print(f"Scraping {stake_addr}... ({i + 1}/{len(processing_records)})")
+
+            # extracting - stake/:stake_id
+            stake_html = get_stake_data_page(stake_addr)
+            stake_dom = BeautifulSoup(stake_html, "html.parser")
+
+            found_result = stake_dom.select_one("div.container-fluid").get_text(strip=True)
+            if "404 - address not found" in found_result:
+                print("    Skipped NOT FOUND")
+                break
+
+            stake_status = stake_dom.select_one("table.table span.badge").get_text(strip=True)
+            if stake_status.lower() == "inactive":
+                print("    Skipped INACTIVE")
+                break
+
+            tx_url = stake_dom.select_one("table.table a")
+            tx_url = tx_url.attrs["href"]
+
+            # extracting - tx/:tx_id
+            tx_html = request(f"https://preprod.cexplorer.io{tx_url}")
+            tx_dom = BeautifulSoup(tx_html, "html.parser")
+            
+            slot_txt = tx_dom.select_one("table.table tr:nth-child(4) span")
+            amount_txt = tx_dom.select_one("table.table tr:nth-child(8) span")
+
+            slot_number = int(slot_txt.get_text().replace(",", ""))
+            ada_amount = float(amount_txt.attrs["title"].replace(",", ""))
+
+            # extracting - stake/:stake_id/asset
+            stake_asset_dom = BeautifulSoup(get_stake_asset_page(stake_addr), "html.parser")
+
+            item_rows = stake_asset_dom.select("div.table-responsive table > thead > tr > td > a")
+            amount_rows = stake_asset_dom.select("div.table-responsive table > thead > tr > td:nth-child(6) > span")
+
+            native_tokens = []
+            for (j, (item_row, amount_row)) in enumerate(zip(item_rows, amount_rows)):
+                asset_name = "\n".join(item_row.get_text().split("\n")[2:-2]).strip()
+                asset_url = item_row.attrs["href"]
+                amount = float(amount_row.attrs["title"].replace(",", ""))
+
+                print(f"    Extracting asset {asset_url}... ({j + 1}/{len(item_rows)})")
+
+                # extracting - asset/:asset_id
+                asset_html = request(f"https://preprod.cexplorer.io{asset_url}")
+                asset_dom = BeautifulSoup(asset_html, "html.parser")
+
+                policy_hash = asset_dom.select_one("div.container-fluid > div > div:nth-child(2) > a")
+                policy_hash = policy_hash.get_text(strip=True)
+
+                native_tokens.append({
+                    "policy_hash": f"0x{policy_hash}",
+                    "asset_name": asset_name,
+                    "amount": amount
+                })
+
+            if stake_addr in formatted_records:
+                print("    Warning OVERRIDDEN STAKE ADDRESS")
+
+            formatted_records[stake_addr] = {
+                "ada_amount": ada_amount,
+                "native_tokens": native_tokens,
+                "slot_number": slot_number
+            }
+
+            break
+        except Exception as e:
+            print(e)
+            
+            if attempt_count >= MAX_ATTEMPT:
+                print(f"    Skipped MAX ATTEMPT REACHED")
+                break
+
+            time.sleep(3)
+            print("Retrying...")
+            attempt_count += 1
+
+# write into a file
+write_file_path = os.path.join(os.path.dirname(__file__), "../cardano-asset-80000000-preprod.json")
+with open(write_file_path, "w") as f:
+    json.dump(formatted_records, f, indent=2)
+
+print("Completed preparing data")
